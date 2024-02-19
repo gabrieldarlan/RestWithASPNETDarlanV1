896f8cb6e81c00c30cd8354eb3e86386c15e9505
gabriel.darlan88@gmail.com <gabriel.darlan88@gmail.com>
2024-02-19 20:38:59 -0300

    migrations

--- RestWithASPNETDarlan/Program.cs
+++ RestWithASPNETDarlan/Program.cs
@@ -4,6 +4,9 @@ using RestWithASPNETDarlan.Business;
 using RestWithASPNETDarlan.Business.Implementation;
 using RestWithASPNETDarlan.Repository;
 using RestWithASPNETDarlan.Repository.Implementation;
+using MySqlConnector;
+using Serilog;
+using EvolveDb;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +16,11 @@ builder.Services.AddControllers();
 
 var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
 builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 31))));
+
+if (builder.Environment.IsDevelopment())
+{
+    MigrateDatabase(connection);
+}
 
 // Versioning API
 builder.Services.AddApiVersioning();
@@ -32,3 +40,22 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+void MigrateDatabase(string connection)
+{
+    try
+    {
+        var evolveConnection = new MySqlConnection(connection);
+        var evolve = new Evolve(evolveConnection, Log.Information)
+        {
+            Locations = new List<string> { "db/migrations", "db/dataset" },
+            IsEraseDisabled = true,
+        };
+        evolve.Migrate();
+    }
+    catch (Exception ex)
+    {
+        Log.Error("Database migration failed", ex);
+        throw;
+    }
+}
